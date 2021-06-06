4724852e15a7f4c46cfe350b692e8cad4705b22d
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2021-06-06 11:01:01 +0300

    refactor: replaced old syntax

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Component1 from './components/Component_1/index';
+import Signature from './components/Signature/index.ts';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Component1 name="Webpack Artem" />
+        <Signature name="Webpack Artem" />
     </React.StrictMode>,
     document.getElementById('root')
 );
 
-if (module && module.hot && module.hot.accept) {
+if (module?.hot?.accept) {
     module.hot.accept();
 }
